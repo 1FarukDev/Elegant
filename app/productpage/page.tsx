029009a27fbb3ddd0c52b39807b2a825c16b6fd0
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-04-17 21:08:14 +0100

    feat:Added the product image

--- app/productpage/page.tsx
+++ app/productpage/page.tsx
@@ -4,8 +4,12 @@ import ELText from "@/components/Atoms/ELText"
 import ArrowRight from '@/public/assets/icons/chevron-right.svg'
 import Image from "next/image"
 import Link from "next/link"
-
-
+import TrayTable from '@/public/assets/images/Tray table.png'
+import firstImage from '@/public/assets/images/Paste Image 1.svg'
+import secondImage from '@/public/assets/images/Paste Image 2.svg'
+import thirdImage from '@/public/assets/images/Paste Image 3.svg'
+import Arrow from '@/public/assets/icons/Previous Button.svg'
+import { useState } from "react"
 
 type menu = {
     name: string
@@ -13,7 +17,14 @@ type menu = {
     to: any
 }
 
+type productImage = {
+    id: any,
+    image: string
+}
+
 const productpage = () => {
+    const [currentIndex, setCurrentIndex] = useState(0)
+
     const Menu = [
         {
             id: '0',
@@ -31,6 +42,34 @@ const productpage = () => {
             to: '/'
         },
     ]
+    const ProductImage = [
+        {
+            id: '0',
+            image: TrayTable
+        },
+        {
+            id: '1',
+            image: firstImage
+        },
+        {
+            id: '2',
+            image: secondImage
+        },
+        {
+            id: '3',
+            image: thirdImage
+        },
+    ]
+    const handleNextImage = () => {
+        if (currentIndex < ProductImage.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        }
+    };
+    const handlePrevForm = () => {
+        if (currentIndex > 0) {
+            setCurrentIndex(currentIndex - 1);
+        }
+    };
     return (
         <main className="container mx-auto">
             <div className="flex gap-4">
@@ -45,6 +84,25 @@ const productpage = () => {
                     )
                 })}
             </div>
+            <section className="flex gap-16">
+                <div className="mt-2 w-1/2 relative">
+                    <Image src={ProductImage[currentIndex].image} alt="Image" className="bg-gray-200 w-full" />
+                    <div className="flex items-center justify-between absolute top-[30%] w-full">
+                        <Image src={Arrow} alt="Arrow Back" className="cursor-pointer" onClick={handlePrevForm}/>
+                        <Image src={Arrow} alt="Arrow Back" className="rotate-180 -mt-5 cursor-pointer" onClick={handleNextImage} />
+                    </div>
+                  <div className="flex gap-6 justify-between mt-6">
+                        {ProductImage.slice(1).map((image: productImage) => {
+                            return (
+                                <main key={image.image} className="w-1/3 border">
+                                    <Image src={image.image} alt={`${image.id} image`} className="w-full"/>
+                                </main>
+                            )
+                        })}
+                  </div>
+                </div>
+                <div className="w-1/2">sda</div>
+            </section>
         </main>
     )
 }
