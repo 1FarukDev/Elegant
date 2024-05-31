fe6509496bc771541164a3a8e3ae3af119f0f9f3
Faruk Ajibade <ajibadefarukyoungprof02@gmail.com>
2024-05-31 15:23:09 +0100

    Fix

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,9 +1,9 @@
 // utils/supabase/client.ts
 import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseUrl: string = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey: string = process.env
-  .NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+  .NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase: SupabaseClient = createClient(
   supabaseUrl,
